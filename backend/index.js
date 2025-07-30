2c1518577c6491500fb8c1c8fe0252785d4af858
23bipin <bipin.bhusal@gmail.com>
2025-07-30 15:09:04 +0300

    removed frontend from repository

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 app.use(cors())
-app.use(express.json()) // ✅ ADD THIS LINE
+app.use(express.json()) 
 app.use(express.static('dist'))
 
 let notes = [
